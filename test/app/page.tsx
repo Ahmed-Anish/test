ca63ea1ac8cb3176c67ad2331e166ac866577e3e
Ahmed Ahmed <hmada2405@gmail.com>
2025-11-24 18:12:40 -0500

    Created Components

--- test/app/page.tsx
+++ test/app/page.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
+import Header from "./header";
+import NonEV from "./nonEV";
+import Rideshare from "./rideshare";
+import ElectricCars from "./electricCars";
+
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center ">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16  sm:items-start">
+        <Header />
         <h1>Ford</h1>
         <h2>(Future Mobility Home)</h2>
+        <ElectricCars />
+        <Rideshare />
+        <NonEV />
       </main>
     </div>
   );
---
+++ test/app/rideshare.tsx
@@ -0,0 +1,4 @@
+export default function Rideshare()
+{
+    return(<></>)
+}
