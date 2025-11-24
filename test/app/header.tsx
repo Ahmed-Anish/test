4382d34dc325f61c8efca7e9adbc8f5ce15e8a74
Saffron <redsavage2x@gmail.com>
2025-11-24 18:22:04 -0500

    Starting header

--- test/app/header.tsx
+++ test/app/header.tsx
@@ -1,4 +1,9 @@
-export default function Header()
-{
-    return(<></>)
+export default function Header() {
+  return <>
+    <nav className="flex space-x-2 bg-zinc-100 rounded-full p-4 text-lg mx-auto *:bg-zinc-300 text-black *:rounded-xl *:p-2 *:cursor-pointer">
+        <a href="#NonEV">Non Electric Vehicles </a>
+        <a href="#RideShare">Ride sharing </a>
+        <a href="#EV">Electrical vehicles </a>
+    </nav>
+  </>;
 }
