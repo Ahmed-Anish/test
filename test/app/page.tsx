d45bbeff9e972289efecff2883298fe5c3215d17
Saffron <redsavage2x@gmail.com>
2025-11-24 20:12:05 -0500

    Update Ride Share

--- test/app/page.tsx
+++ test/app/page.tsx
@@ -4,25 +4,22 @@ import Header from "./header";
 import NonEV from "./nonEV";
 import Rideshare from "./rideshare";
 import ElectricCars from "./electricCars";
-import { useState,useEffect } from "react";
-
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [theme,setTheme] = useState<boolean>(false);
-    useEffect(() => {
-    if (theme) {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
-    }
+  const [theme, setTheme] = useState<boolean>(false);
+  useEffect(() => {
+    document.body.classList.toggle("dark", theme);
   }, [theme]);
 
   return (
     <main className="flex flex-col space-y-8 p-1 h-screen">
-        <Header theme={theme} setTheme={setTheme}/>
+      <Header theme={theme} setTheme={setTheme} />
+      <div className="flex space-x-2">
         <ElectricCars />
-        <Rideshare />
         <NonEV />
+      </div>
+      <Rideshare />
     </main>
   );
 }
