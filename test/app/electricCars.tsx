fd33310b3772c3e877e84ec4bb972d14866be1cd
Ahmed Ahmed <hmada2405@gmail.com>
2025-11-24 19:21:12 -0500

    Finished ElectricCars Component

--- test/app/electricCars.tsx
+++ test/app/electricCars.tsx
@@ -1,4 +1,49 @@
-export default function ElectricCards()
+"use client";
+import 
 {
-    return(<></>)
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts"
+
+const data = 
+[
+    {name: "LCE", impact: 95},
+    {name: "EE", impact: 90},
+    {name: "Less Noise", impact: 95},
+    {name: "Air Quality", impact: 85},
+    {name: "Fuel Savings", impact: 80}
+];
+
+export default function ElectricCars()
+{
+    return(
+        <section className="w-[100%] max-w-xl mt-15">
+            <h2 className="h-44 w-full bg-white/10 rounded-xl p-3 text-center text-4xl font-mono">
+                Societal Benefits of Electric Cars
+                <p className="text-sm text-center mt-5">
+                    Impacts such as Lower Carbon Emissions, Energy Efficiency, Less Noise, Air Quality, & Fuel Savings 
+                    are taken into consideration
+                </p>
+            </h2>
+            <div className="h-72 w-full bg-white/5 rounded-xl p-5"> {/* Our bar graph that shows the data */}
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart data={data}>
+                        <CartesianGrid strokeDasharray="1 3"/>
+                            <XAxis dataKey="name"/>
+                            <YAxis />
+                            <Tooltip />
+                            <Bar dataKey="impact" fill="#193cb8"/>
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+            <p className="text-sm text-center mt-4 text-black font-mono">
+                These scores represents the advantages through the use of electric cars! 
+            </p>
+        </section>
+    );
 }
